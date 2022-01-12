e3a868c9a25edd2294b7eb254aa3cc5325b745e3
Anubhav Kini <anubhavkini@pm.me>
2022-01-12 15:25:33 +0530

    Implemented basic encryption

--- driver.py
+++ driver.py
@@ -1,8 +1,14 @@
 from treeParityMachine import tpm
+import cryptography
+import os
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet
+import base64
 
-N = 4      # Number of input neurons per hidden neuron
-K = 128    # Number of neurons in hidden layer
-L = 1      # Range of values that weights can take {-L, ..., L}
+N = 101      # Number of input neurons per hidden neuron
+K = 2        # Number of neurons in hidden layer
+L = 3        # Range of values that weights can take {-L, ..., L}
 
 # Create two tree parity machines
 a = tpm(N, K, L)
@@ -32,13 +38,31 @@ while True and count < 500:
         b.HebbianLearning(b.output, a.output)
     count += 1
 
-# Print final weights
-#print(a.weights)
-#print(b.weights)
-
 # Check whether weights are the same
 for i in range(len(a.weights)):
     if a.weights[i] != b.weights[i]:
         print("Weights have not synced.")
         exit()
 print("Weights have synced.")
+
+# Create 
+l = ''.join([str(x) for x in a.weights])
+
+salt = os.urandom(16)
+
+kdf = PBKDF2HMAC(
+    algorithm = hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=390000,
+)
+
+key = base64.urlsafe_b64encode(kdf.derive(l.encode()))
+print(key)
+
+f = Fernet(key)
+
+token = input("Enter text: ")
+
+print("E:", f.encrypt(token.encode()))
+print("D:", f.decrypt(f.encrypt(token.encode())).decode())
